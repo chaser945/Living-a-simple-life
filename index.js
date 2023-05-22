77975ed7f0ee6703c8c17d7801598c94d914c52e
chaser945 <113222684+chaser945@users.noreply.github.com>
2023-05-22 10:01:48 +0500

    fixed navbar

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-const hamburger = document.querySelector('.hamburger')
-const mobileNav = document.querySelector('.mobile-nav')
-const bars = document.querySelectorAll('.bar')
-const mobileNavLinks = document.querySelectorAll('.mobile-nav-li')
+// const hamburger = document.querySelector('.hamburger')
+// const mobileNav = document.querySelector('.mobile-nav')
+// const bars = document.querySelectorAll('.bar')
+// const mobileNavLinks = document.querySelectorAll('.mobile-nav-li')
 
-hamburger.addEventListener('click', function () {
+// hamburger.addEventListener('click', function () {
 
-    console.log(mobileNavLinks)
-    if (!mobileNav.classList.contains('is-active')) {
-        mobileNav.classList.add('is-active')
-    } else {
-        mobileNav.classList.remove('is-active')
-    }
+//     console.log(mobileNavLinks)
+//     if (!mobileNav.classList.contains('is-active')) {
+//         mobileNav.classList.add('is-active')
+//     } else {
+//         mobileNav.classList.remove('is-active')
+//     }
 
-    for (let i = 0; i < bars.length; i++) {
-
-
-        if (!bars[i].classList.contains('is-active')) {
-            bars[i].classList.add('is-active')
-        }
-        else {
-            bars[i].classList.remove('is-active')
-        }
-    }
-
-    // mobileNavLinks.forEach((link) => {
-    //     link.addEventListener('click', function () {
-    //         mobileNav.classList.remove('is-active')
-    //         bars.forEach((bar) => {
-    //             bar.classList.remove('is-active')
-    //         })
-    //     })
-    // })
+//     for (let i = 0; i < bars.length; i++) {
 
 
-})
+//         if (!bars[i].classList.contains('is-active')) {
+//             bars[i].classList.add('is-active')
+//         }
+//         else {
+//             bars[i].classList.remove('is-active')
+//         }
+//     }
+
+//     mobileNavLinks.forEach((link) => {
+//         link.addEventListener('click', function () {
+//             mobileNav.classList.remove('is-active')
+//             bars.forEach((bar) => {
+//                 bar.classList.remove('is-active')
+//             })
+//         })
+//     })
+
+
+// })
